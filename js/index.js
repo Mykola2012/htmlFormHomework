73bd9055d73c23429264a7e91d8845588cb5f779
Revika Mukola <revikanikolaj@gmail.com>
2022-02-22 16:05:21 +0100

    Add validation to form

--- js/index.js
+++ js/index.js
@@ -9,3 +9,16 @@ function calcVolumeBoll(e) {
 
   pBollResultEl.textContent = `${(4 / 3) * Math.PI * e.target.value ** 3}`;
 }
+
+const textareaEl = document.querySelector("textarea");
+
+textareaEl.oninput = (e) => {
+  const TEXTAREA_REG_EXP = /^\S+$/;
+  if (TEXTAREA_REG_EXP.test(e.target.value)) {
+    e.target.classList.add("valid");
+    e.target.classList.remove("invalid");
+  } else {
+    e.target.classList.remove("valid");
+    e.target.classList.add("invalid");
+  }
+};
